655152018a944203c41ccad53113015905b0ffef
dongguacute <gudupao2022@gmail.com>
2026-06-16 22:09:17 +0800

    refactor(types): 移除未使用的 Output 类型

--- packages/core/src/types/url.ts
+++ packages/core/src/types/url.ts
@@ -1,8 +1,5 @@
+// 输入歌单或者专辑url和平台类型
 export type Input = {
     url: string;
     provider: "QQMusic" | "NetEaseMusic" | "KugouMusic" | "AppleMusic" | "Spotify" | "YTMusic";
-}
-
-export type Output = {
-   content: string;
 }
